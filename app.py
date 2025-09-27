d1cd63b07d62df6d2ff49f4f74435d69c994388a
azakg <azakgzuu@gmail.com>
2025-09-27 11:48:48 -0400

    Учимся играться с @app.route

--- app.py
+++ app.py
@@ -6,9 +6,13 @@ app = Flask(__name__)
 def index():
     return "First page"
 
-@app.route("/hello")
-def hello():
-    return "Hello page"
+@app.route("/hello/<name>")
+def hello(name):
+    return f"Hello, {name}!"
+
+@app.route("/square/<int:n>")
+def square(n: int):
+    return f"{n}^2 = {n*n}"
 
 if __name__ == '__main__':
     app.run(debug=True)
