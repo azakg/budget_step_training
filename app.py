977e48d6685b2fe1f78e39379bcecb43c03a0d82
azakg <azakgzuu@gmail.com>
2025-09-27 11:40:47 -0400

    Flask Initial steps

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "First page"
+
+@app.route("/hello")
+def hello():
+    return "Hello page"
+
+if __name__ == '__main__':
+    app.run(debug=True)
