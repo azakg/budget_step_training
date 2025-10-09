119aa9790fc9e3c581f865689457040a880b4633
azakg <azakgzuu@gmail.com>
2025-10-09 17:55:42 -0400

    100925

--- app.py
+++ app.py
@@ -1,18 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template, request, url_for
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def index():
-    return "First page"
+    # передаём переменные в шаблон
+    title = "Мой первый Flask + Bootstrap"
+    return render_template("index.html", title=title)
 
-@app.route("/hello/<name>")
-def hello(name):
-    return f"Hello, {name}!"
+@app.route("/sum")
+def sum_ab():
+    a = request.args.get("a", type=float, default=0)
+    b = request.args.get("b", type=float, default=0)
+    result = a + b
+    # покажем результат на той же странице (или отдельной)
+    return render_template(
+        "index.html",
+        title="Суммируем числа",
+        a=a, b=b, result=result
+    )
 
-@app.route("/square/<int:n>")
-def square(n: int):
-    return f"{n}^2 = {n*n}"
-
-if __name__ == '__main__':
+if __name__ == "__main__":
+    print(app.url_map)  # полезно видеть зарегистрированные маршруты
     app.run(debug=True)
